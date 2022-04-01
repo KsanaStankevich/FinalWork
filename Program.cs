26330d2b6c72e51c444dcf7d06d76f890856bf0f
ksana stankevich <ksanastankevich@gmail.com>
2022-04-01 11:52:30 +0300

    Переписали цикл под подсчет элементов нового массива

--- Program.cs
+++ Program.cs
@@ -8,11 +8,22 @@
 // ["1234", "1567", "-2", "computer science"] -> ["-2"]
 // ["Russia", "Denmark", "Kazan"] -> []
 
-string[] mass = { "1234", "б", "1567", "-2", "computer science", "ап" };
+string[] mass = { "1234", "б", "1567", "-2", "computer science", "ап", "djhdejj"};
 // int length = mass[3].Length;
+// string[] newmass = new String[];
+int counter = 0;
 
-for (int i = 0; i < mass.Length; i++)
+for (int i = 0; i < mass.Length; i++) //Считаем сколько элементов будет в новом массиве
 {
     int length = mass[i].Length;
-    if (length >= 3) Console.Write(mass[i] + " ; ");
+    if (length >= 3) counter++;
 }
+Console.WriteLine(counter);
+
+
+
+// for (int i = 0; i < mass.Length; i++)
+// {
+//     int length = mass[i].Length;
+//     if (length >= 3) Console.Write(mass[i] + " ; ");
+// }
